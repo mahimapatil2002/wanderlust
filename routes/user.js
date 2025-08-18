f5a7dc10dce389c851e1cccf8d2ab2e73e2c7924
mahimapatil2002 <patilmahima0502@gamil.com>
2025-08-18 12:38:19 +0530

    add controller function

--- routes/user.js
+++ routes/user.js
@@ -5,54 +5,25 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
+const usersController = require("../controller/users.js")
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs");
-});
+
+router.get("/signup",(usersController.renderSignupForm));
 
 //signup
-router.post("/signup",wrapAsync(async (req,res)=>{
-    try{
-        let {username,email,password} = req.body;
-        const newUser =new User({email,username});
-        const registerUser = await User.register(newUser,password);
-        console.log(registerUser);
-        req.login(registerUser,(err) => {
-            if(err) {
-                return next(err);
-            }
-            req.flash("success","Welcome to Wanderlust");
-            res.redirect("/listings");
-        });
-    } catch(e){
-        req.flash("error", e.message);
-        res.redirect("/signup");
-    }  
-}));
+router.post("/signup",wrapAsync (usersController.signup));
 
 //login
-router.get("/login",(req,res)=>{
-    res.render("users/login.ejs")
-});
+router.get("/login",(usersController.renderLoginForm));
 
 router.post("/login", 
     saveRedirectUrl,
-    passport.authenticate("local",{  failureRedirect: '/login',failureFlash: true  }),
-    async (req,res)=>{
-        req.flash("success","Welcome back to Wanderlust! ");
-        let redirectUrl = res.locals.redirectUrl || "/listings";
-        res.redirect(redirectUrl);
-});
+    passport.authenticate("local",
+        {  failureRedirect: '/login',failureFlash: true  }
+    ),
+    (usersController.login));
 
 //logout rout
-router.get("/logout",(req,res,next) => {
-    req.logout((err) =>{
-        if(err){
-           return next(err);
-        }
-        req.flash("success", "you are logged out!");
-        res.redirect("/listings");
-    });
-});
+router.get("/logout",(usersController.logout));
 
 module.exports = router;
