0350cb5f70faa22c86f4f6bd83d65602962eef14
mahimapatil2002 <patilmahima0502@gamil.com>
2025-08-19 09:56:21 +0530

    update image

--- routes/listing.js
+++ routes/listing.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const Listing = require("../models/listing.js");
 const wrapAsync =require("../utils/wrapAsync.js");
 const {isLoggedIn , isOwner,validateListing ,} = require("../middleware.js"); 
-
-
 const listingController = require("../controller/listings.js");
+const multer  = require('multer');
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 
 //index route 
@@ -18,14 +19,14 @@ router.get("/new", isLoggedIn,(listingController.renderNewForm));
 router.get("/:id", wrapAsync (listingController.showListing));
 
 //create route
-router.post("/",isLoggedIn, validateListing, wrapAsync (listingController.createListing));
-
+router.post("/",isLoggedIn, upload.single("listing[image]"),validateListing, wrapAsync (listingController.createListing));
+// Using multer for file upload and stor the image in the uploads folder
 
 //edit rout
 router.get("/:id/edit",isLoggedIn, isOwner, wrapAsync (listingController.renderEditForm));
 
 //update route
-router.put("/:id",isLoggedIn, isOwner, validateListing, wrapAsync (listingController.updateListing));
+router.put("/:id",isLoggedIn, isOwner,upload.single("listing[image]"), validateListing, wrapAsync (listingController.updateListing));
 
 //delete route
 router.delete("/:id", isLoggedIn, isOwner, wrapAsync (listingController.deleteListing));
