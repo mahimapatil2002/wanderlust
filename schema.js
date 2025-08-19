54a5dd204d1e53ad83bea84b3af1eaae80b9f0d6
mahimapatil2002 <patilmahima0502@gamil.com>
2025-08-19 10:04:09 +0530

    update image joi schema

--- schema.js
+++ schema.js
@@ -8,9 +8,8 @@ module.exports.listingSchema = Joi.object({
         country: Joi.string().required(),
         price: Joi.number().required().min(0),
        image: Joi.object({
-    url: Joi.string().uri({ scheme: ['http', 'https'] }).allow("", null),
-    filename: Joi.string().allow("", null)
-  }).required()
+            url: Joi.string().required(),
+            filename: Joi.string().required(),}).optional(),
     }).required().unknown(true),
 }); 
 
